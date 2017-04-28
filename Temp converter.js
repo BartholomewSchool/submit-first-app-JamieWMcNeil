0b56b987cc582127c237baf7ca456b6356036ff6
JamieWMcNeil <cheesechum91@gmail.com>
2017-04-29 00:46:16 +0100

    Update and rename .gitignore to Temp converter.js

---
+++ Temp converter.js
@@ -0,0 +1,22 @@
+//there may be a problem with some of the code and possible mistakes i was struggling to test it
+//there may also be a problem with how i have uploaded it
+
+<html>
+<body>
+
+<script language="JavaScript" type ="text/Javascript">
+
+//variable and prompt to enter the temperature and record it
+var degCentigrade = prompt("Enter degrees in centigrade",0);
+
+//variable and math to convert the temperature
+var degFahrenheit = 9 / 5 * degCentigrade + 32;
+
+//end result that displays previous and current temperature after convertion
+document.write(degCentigrade + " degrees centigrade is " + degFahrenheit +
+.	" degrees Fahrenheit");
+
+</script>
+
+</body>
+</html>
